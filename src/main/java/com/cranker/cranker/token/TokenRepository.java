8b6a239e5bfeb064156b883e9d4ac17b02445b40
moldovexc <alexanderparpulansky@gmail.com>
2024-01-28 03:01:30 +0200

    Added token related classes and functionalities using redis

---
+++ src/main/java/com/cranker/cranker/token/TokenRepository.java
@@ -0,0 +1,7 @@
+package com.cranker.cranker.token;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TokenRepository extends CrudRepository<Token, String> {}
