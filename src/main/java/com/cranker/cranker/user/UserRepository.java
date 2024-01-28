b46e51102c175630cd4e3b56d3d1a089102bc30d
moldovexc <alexanderparpulansky@gmail.com>
2024-01-28 02:58:13 +0200

    Added mail confirmation query to update user email status

--- src/main/java/com/cranker/cranker/user/UserRepository.java
+++ src/main/java/com/cranker/cranker/user/UserRepository.java
@@ -1,6 +1,9 @@
 package com.cranker.cranker.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -9,5 +12,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByEmailIgnoreCase(String email);
 
     Boolean existsByEmailIgnoreCase(String email);
-
+    @Transactional
+    @Modifying
+    @Query("UPDATE User u " +
+            "SET u.isVerified = TRUE WHERE u.email = ?1")
+    void confirmEmail(String email);
 }
