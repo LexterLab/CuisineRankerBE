fe4473f06b014e1f720755525a29dde27818c9d8
moldovexc <alexanderparpulansky@gmail.com>
2024-02-03 15:17:12 +0200

    Added new api message for wrong token type

--- src/main/java/com/cranker/cranker/utils/Messages.java
+++ src/main/java/com/cranker/cranker/utils/Messages.java
@@ -2,6 +2,7 @@ package com.cranker.cranker.utils;
 
 public interface Messages {
     String USER_SUCCESSFULLY_REGISTERED = "Your Account has been created successfully.";
+    String PASSWORDS_DONT_MATCH = "Password don't match";
     String USER_NOT_FOUND_WITH_EMAIL = "User not found with email: ";
     String EMAIL_EXISTS = "Email already exists!";
     String INVALID_JWT_TOKEN = "{\"message\": \"Invalid JWT Token\"}";
@@ -12,4 +13,5 @@ public interface Messages {
     String NOT_PERMITTED = "User has insufficient permissions to access";
     String TOKEN_ALREADY_CONFIRMED = "Token was already confirmed";
     String EMAIL_ALREADY_CONFIRMED = "Email was already confirmed";
+    String WRONG_TOKEN_TYPE = "Provided token is not for this type of operation";
 }
