570eeb76f32d4eeb69a04ec7860be4b45f13875e
moldovexc <alexanderparpulansky@gmail.com>
2024-02-13 21:04:42 +0200

    Setting up h2 for testing

--- src/main/java/com/cranker/cranker/bootstrap/TestDataLoader.java
+++ src/main/java/com/cranker/cranker/bootstrap/TestDataLoader.java
@@ -9,14 +9,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
-@Profile("docker")
-public class DataCommandLineRunner implements CommandLineRunner {
+@Profile("test")
+public class TestDataLoader implements CommandLineRunner {
     private final RoleRepository roleRepository;
     private final UserRepository userRepository;
-
     @Override
     public void run(String... args) {
-        if (roleRepository.count() == 0 && userRepository.count() == 0) {
+        if(roleRepository.count() == 0 && userRepository.count() == 0) {
             BootstrapHelper.setUp(roleRepository, userRepository);
         }
     }
