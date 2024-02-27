1dfe2e59a4663d1d6b28aa9924669a22c4108cc1
moldovexc <alexanderparpulansky@gmail.com>
2024-02-27 20:17:04 +0200

    Removed helper repo for now

--- src/main/java/com/cranker/cranker/recipe/RecipeRepository.java
+++ src/main/java/com/cranker/cranker/recipe/RecipeRepository.java
@@ -1,7 +1,9 @@
 package com.cranker.cranker.recipe;
 
-import app.tozzi.repository.JPASearchRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RecipeRepository extends JpaRepository<Recipe, Long>, JPASearchRepository<Recipe> {
+import java.util.List;
+
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    List<Recipe> findAllByUserEmail(String email);
 }
