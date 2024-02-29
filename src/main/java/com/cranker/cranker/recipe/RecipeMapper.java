ac608428ce7e301bc44148412b2a1c36a7a80d82
moldovexc <alexanderparpulansky@gmail.com>
2024-02-29 09:32:39 +0200

    Mapped prep time + cook time to totalTime in recipe payload

--- src/main/java/com/cranker/cranker/recipe/RecipeMapper.java
+++ src/main/java/com/cranker/cranker/recipe/RecipeMapper.java
@@ -2,6 +2,7 @@ package com.cranker.cranker.recipe;
 
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -9,6 +10,8 @@ import java.util.List;
 @Mapper(injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface RecipeMapper {
     RecipeMapper INSTANCE = Mappers.getMapper(RecipeMapper.class);
+    @Mapping(expression = "java(recipe.getPrepTimeInMinutes() + recipe.getCookTimeInMinutes())", target = "totalTime")
+    RecipeDTO entityToDTO(Recipe recipe);
 
     List<RecipeDTO> entityToDTO(Iterable<Recipe> recipes);
 }
