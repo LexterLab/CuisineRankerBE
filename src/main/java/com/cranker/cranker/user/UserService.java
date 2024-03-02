edf3f1b9acea71897cf04f45b46ce06054a61ba1
moldovexc <alexanderparpulansky@gmail.com>
2024-03-02 20:09:01 +0200

    business logic for modifying user's personal info

--- src/main/java/com/cranker/cranker/user/UserService.java
+++ src/main/java/com/cranker/cranker/user/UserService.java
@@ -1,6 +1,8 @@
 package com.cranker.cranker.user;
 
 import com.cranker.cranker.exception.ResourceNotFoundException;
+import com.cranker.cranker.user.payload.UserDTO;
+import com.cranker.cranker.user.payload.UserRequestDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +15,14 @@ public class UserService {
         User user = repository.findUserByEmailIgnoreCase(email)
                 .orElseThrow(() -> new ResourceNotFoundException("User", "email", email));
         return UserResponseMapper.INSTANCE.entityToDTO(user);
+    }
+
+    public UserRequestDTO changeUserPersonalInfo(String email, UserRequestDTO requestDTO) {
+        User user = repository.findUserByEmailIgnoreCase(email)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "email", email));
+
+        UserResponseMapper.INSTANCE.updateUserFromDto(requestDTO, user);
+        user.setFirstName(requestDTO.firstName());
+        return UserResponseMapper.INSTANCE.entityToRequestDTO(repository.save(user));
     }
 }
