685fcf1f299db021d1abea2c3a8f8ed2b4009576
moldovexc <alexanderparpulansky@gmail.com>
2024-03-10 14:19:24 +0200

    Adding token related logs

--- src/main/java/com/cranker/cranker/token/TokenHelper.java
+++ src/main/java/com/cranker/cranker/token/TokenHelper.java
@@ -3,6 +3,8 @@ package com.cranker.cranker.token;
 import com.cranker.cranker.exception.APIException;
 import com.cranker.cranker.utils.Messages;
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
@@ -13,13 +15,15 @@ import java.time.LocalDateTime;
 public class TokenHelper {
 
     private TokenRepository tokenRepository;
-
+    private final Logger logger = LogManager.getLogger(this);
     private void validateToken(Token token, TokenType type){
         if (token.getConfirmedAt() != null) {
+            logger.error(Messages.TOKEN_ALREADY_CONFIRMED + ": {}", token.getValue());
             throw new APIException(HttpStatus.BAD_REQUEST, Messages.TOKEN_ALREADY_CONFIRMED);
         }
 
         if(!token.getType().equalsIgnoreCase(type.getName())) {
+            logger.error(Messages.WRONG_TOKEN_TYPE + ": {}", token.getType());
             throw new APIException(HttpStatus.BAD_REQUEST, Messages.WRONG_TOKEN_TYPE);
         }
     }
