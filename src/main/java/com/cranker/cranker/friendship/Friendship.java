455a2cfc7e463da079cbe1b16553462bb03dfb39
moldovexc <alexanderparpulansky@gmail.com>
2024-04-20 23:26:16 +0300

    Friendship model and status

---
+++ src/main/java/com/cranker/cranker/friendship/Friendship.java
@@ -0,0 +1,48 @@
+package com.cranker.cranker.friendship;
+
+import com.cranker.cranker.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Table(name = "friendships")
+public class Friendship {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter(AccessLevel.NONE)
+    @Column(nullable = false)
+    private String status;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "friend_id")
+    private User friend;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        updatedAt = LocalDateTime.now();
+    }
+
+    public void setFriendshipStatus(FriendshipStatus friendshipStatus) {
+        this.status = friendshipStatus.getName();
+    }
+}
