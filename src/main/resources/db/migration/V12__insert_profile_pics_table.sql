55053e1a5e07e3dc14f1944d859cf454b99d9ec9
moldovexc <alexanderparpulansky@gmail.com>
2024-04-22 19:26:11 +0300

    Fixed image urls

--- src/main/resources/db/migration/V12__insert_profile_pics_table.sql
+++ src/main/resources/db/migration/V12__insert_profile_pics_table.sql
@@ -1,7 +1,7 @@
 INSERT INTO profile_pics (name, url, category_id)
 VALUES
-    ('Rattingam', 'https://bit.ly/49PJgT3', (SELECT id FROM profile_pic_categories WHERE name = 'STARTER')),
-    ('Billy', 'https://bit.ly/49Mo93R', (SELECT id FROM profile_pic_categories WHERE name = 'STARTER')),
-    ('Jasmine', 'https://bit.ly/3WaCGTY', (SELECT id FROM profile_pic_categories WHERE name = 'STARTER')),
-    ('William', 'https://bit.ly/4aJQN7f', (SELECT id FROM profile_pic_categories WHERE name = 'STARTER'))
+    ('Rattingam', 'https://storage.googleapis.com/cuisine-media/rat1.jpg', (SELECT id FROM profile_pic_categories WHERE name = 'STARTER')),
+    ('Billy', 'https://storage.googleapis.com/cuisine-media/male1.png', (SELECT id FROM profile_pic_categories WHERE name = 'STARTER')),
+    ('Jasmine', 'https://storage.googleapis.com/cuisine-media/female4.png', (SELECT id FROM profile_pic_categories WHERE name = 'STARTER')),
+    ('William', 'https://storage.googleapis.com/cuisine-media/cat3.png', (SELECT id FROM profile_pic_categories WHERE name = 'STARTER'))
 ON CONFLICT DO NOTHING;
