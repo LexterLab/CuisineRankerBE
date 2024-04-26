523e7039e5a54dd2b58c209a1ffe8d004f43cfda
moldovexc <alexanderparpulansky@gmail.com>
2024-04-27 01:14:00 +0300

    Added missing logger to retrieving friend list

--- src/main/java/com/cranker/cranker/user/UserService.java
+++ src/main/java/com/cranker/cranker/user/UserService.java
@@ -83,6 +83,7 @@ public class UserService {
             }
         }
 
+        logger.info("Retrieving friends for: {}", email);
         return FriendshipMapper.INSTANCE.pageToFriendshipResponse(friendshipPage, friendships);
     }
 }
