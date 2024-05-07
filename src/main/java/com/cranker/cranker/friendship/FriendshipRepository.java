67f9e808ca752e3b02b8a6639589fac48f988a86
moldovexc <alexanderparpulansky@gmail.com>
2024-05-08 01:10:55 +0300

    repository methods for friend request logic

--- src/main/java/com/cranker/cranker/friendship/FriendshipRepository.java
+++ src/main/java/com/cranker/cranker/friendship/FriendshipRepository.java
@@ -10,4 +10,13 @@ public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
             "WHERE (f.user.id = :id OR f.friend.id = :id) " +
             "AND f.status = 'Active' ")
     Page<Friendship> findAllFriendsByUserId(Long id, Pageable pageable);
+
+    @Query("SELECT COUNT(f) > 0 FROM Friendship f " +
+            "WHERE f.status = :status " +
+            "AND ((f.user.id = :id AND f.friend.id = :friendId) OR (f.user.id = :friendId AND f.friend.id = :id)) ")
+    boolean friendshipExists(Long id, Long friendId, String status);
+
+    @Query("SELECT f FROM Friendship f WHERE  f.friend.id = :id " +
+            "AND f.status = 'Pending' ")
+    Page<Friendship> findAllFriendRequests(Long id, Pageable pageable);
 }
