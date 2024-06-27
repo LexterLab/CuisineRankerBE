3e8d7cd80728ea764f78c218591b1827012048ac
moldovexc <alexanderparpulansky@gmail.com>
2024-06-28 01:25:21 +0300

    Added active profiles to be set to test when testing integration related tests

--- src/test/java/com/cranker/cranker/integration/BaseIntegrationTest.java
+++ src/test/java/com/cranker/cranker/integration/BaseIntegrationTest.java
@@ -2,6 +2,7 @@ package com.cranker.cranker.integration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -9,6 +10,7 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
+@ActiveProfiles("test")
 public class BaseIntegrationTest {
 
     @Container
