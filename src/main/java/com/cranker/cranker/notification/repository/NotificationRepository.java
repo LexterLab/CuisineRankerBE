7d9c4ebd92014e62a01e6993e1fff70efd54d4d1
moldovexc <alexanderparpulansky@gmail.com>
2024-07-14 21:57:45 +0300

    fixed NotificationRepository to work with long instead of int

--- src/main/java/com/cranker/cranker/notification/repository/NotificationRepository.java
+++ src/main/java/com/cranker/cranker/notification/repository/NotificationRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
     Page<Notification> findAllByUser(User user, Pageable pageable);
 }
