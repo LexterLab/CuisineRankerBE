60e0eb6c02caac13f228cdc9f102de080e53495f
moldovexc <alexanderparpulansky@gmail.com>
2024-07-16 21:07:46 +0300

    Added builder to notification request body

--- src/main/java/com/cranker/cranker/notification/payload/NotificationRequestDTO.java
+++ src/main/java/com/cranker/cranker/notification/payload/NotificationRequestDTO.java
@@ -1,18 +1,10 @@
 package com.cranker.cranker.notification.payload;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import lombok.Builder;
 
-
+@Builder
 public record NotificationRequestDTO(
-        @Schema(example = "Friendship request")
-        @NotBlank(message = "Field title cannot be blank")
-        @Size(min = 4, max = 50, message = "Field title must be 4-30 characters")
         String title,
-        @Schema(example = "You've received friendship request from Antonio")
-        @NotBlank(message = "Field message cannot be blank")
-        @Size(message = "Field message must be 4-50 characters", max = 50, min = 4)
         String message
 ) {
 }
