78b9af95502af9fa472e4666b8c13fb6bcc2fb0a
moldovexc <alexanderparpulansky@gmail.com>
2024-09-08 21:48:00 +0300

    Add heartbeat scheduler to websocket

--- src/main/java/com/cranker/cranker/config/WebSocketConfig.java
+++ src/main/java/com/cranker/cranker/config/WebSocketConfig.java
@@ -1,7 +1,9 @@
 package com.cranker.cranker.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -12,12 +14,25 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket").withSockJS();
+        registry.addEndpoint("/websocket")
+                .setAllowedOrigins("http://localhost:3000")
+                .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/topic")
+                .setTaskScheduler(heartbeatTaskScheduler())
+                .setHeartbeatValue(new long[]{4000, 4000});
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Bean
+    public ThreadPoolTaskScheduler heartbeatTaskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(1);
+        taskScheduler.setThreadNamePrefix("wss-heartbeat-thread-");
+        taskScheduler.initialize();
+        return taskScheduler;
     }
 }
